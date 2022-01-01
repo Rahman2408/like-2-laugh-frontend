fda01986298081ab90561567953489ff007a0049
Rahman Elsirafy <rahman2408@gmail.com>
2022-01-01 14:25:35 -0500

    Creates routes for existing components and creates basic component for what will be an individual show page for jokes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import JokeList from './containers/JokeList';
-
+import {Switch, Route} from 'react-router-dom'
+import JokeShow from './containers/JokeShow';
 
 function App() {
   return (
     <div className="main">
       <header className="header">
-       iLike-2-Laugh
+       <h1>iLike-2-Laugh</h1>
       </header>
-      <h1>Some Jokes</h1>
-      <JokeList />
+      <Switch>
+        <Route path="/jokes/:id">
+          <JokeShow />
+        </Route>
+
+        <Route path="/jokes">
+          <h2>All Jokes:</h2>
+          <JokeList />
+        </Route>
+
+      </Switch>
     </div>
   );
 }
---
+++ src/containers/JokeShow.js
@@ -0,0 +1,3 @@
+export default function JokeShow(props){
+  return <p>joke show page</p>
+}
