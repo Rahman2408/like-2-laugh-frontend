e5667c33e2fc75137bf63683aab107392c94c77d
Rahman Elsirafy <rahman2408@gmail.com>
2022-01-01 10:14:58 -0500

    imports and adds use effect from react

--- src/containers/JokeList.js
+++ src/containers/JokeList.js
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 function JokeList(props) {
+
+  useEffect(() => console.log('useEffect is working'))
+
   return <h3>Presentational component to render jokes here</h3>
 }
 
