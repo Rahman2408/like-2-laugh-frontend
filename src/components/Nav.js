f1593089a3097f39686ea6b134a3c8cec426cca5
Rahman Elsirafy <rahman2408@gmail.com>
2022-01-02 00:22:32 -0500

    Distinguishes and adjusts the navigation options based on whether a user is logged in or not

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,19 @@ import { NavLink } from "react-router-dom";
 import { connect } from  "react-redux";
 import { logout } from "../redux/actionCreator"
 
-function Nav({logout}) {
- return <nav>
+function Nav({logout, username}) {
+
+  const activeUser = () => <nav>
     <NavLink to="/jokes"><button>All Jokes</button></NavLink>
     <NavLink to={"/myjokes"}><button> My Jokes</button></NavLink>
     <button onClick={logout}> Log Out</button>
   </nav>
+  
+  const inactiveUser = () => <nav>Log In or Sign Up to get LOLin'</nav>
+
+  return username ? activeUser() : inactiveUser()
 }
-export default connect(null, {logout})(Nav);
+
+const mapStateToProps = (state) => ({username: state.user.username})
+
+export default connect(mapStateToProps, {logout})(Nav);
